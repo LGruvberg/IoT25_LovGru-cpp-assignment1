571a0b22bca1141ce733e93d485f9eac01924fde
Lovelej <love.gruvberg@gmail.com>
2025-10-15 09:19:54 +0200

    Här lägger jag upp den nya meny-strukturen

--- assignment01/main.cpp
+++ assignment01/main.cpp
@@ -11,93 +11,84 @@ std::string symbol;
 std::string operNames[4] = { "Addition", "Subtraktion", "Multiplikation", "Division" };
 std::string operSymbol[4] = { "+", "-", "x", "/" };
 
-void menyUI() {
-    int val;
-    std::cout   <<  "Meny | Välj mellan alternativen 1 till 5\n"
-        << "\t1. " << operNames[0] << "\n"  //  Cout:ar arrayens första strängvariabel (dvs 0) som ju är 'Addition'
-        << "\t2. " << operNames[1] << "\n"  //  osv...
-        << "\t3. " << operNames[2] << "\n"
-        << "\t4. " << operNames[3] << "\n"
-        << "\t5. Avsluta\n\n"
-        << "\tAlternativ:\t";
-    }
-std::string inputOperations() {
-    oper    = operNames[val-1];     // användarinput (exempelvis 'val = 1') subtraheras med 1 och blir 0, enligt "array-ordning", 0 (oper = "Addition")
-    symbol  = operSymbol[val-1];    // -||-
-    
-    std::cout   << oper << ", tal 1:\t";
-    std::cin    >> input1;
-    std::cout   << oper << ", tal 2:\t" << symbol << " ";
-    std::cin    >> input2;
-    return oper;
-}
+void felInput();
+void minimenyA();   //  För att kunna ha mina minimenyfunktioner i botten - istället för här uppe - så måste jag först kalla
+void minimenyB();   //  på dem här i början (förbereda C++ i förväg, eftersom att kodraderna exekveras uppifrån och nedåt)
+
 int main() {
+    int menyVal;
     while (true) {
-        menyUI(); // Kalla funktion menyUI
-        std::cin >> val;
-        
-        if (std::cin.fail() || val < 1 || val > 5) { // Här hamnar vi vid felaktig input (t ex 'abc') ELLER om input är mindre än 1 ELLER större än 5
-            std::cout   << "::::::::::::::::::::::::::::::::::::::::\n"
-                        << "Det där är inte en siffra mellan 1-5 :/\n\t"
-                        << "Försök igen\n"
-                        << "::::::::::::::::::::::::::::::::::::::::\n";
-            std::cin.clear();   // Reset:ar flag för "dålig character input" 
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
-/* Ledsen för GIGANTISKT kommentarsblock (ville bara dokumentera min uppfattning av 
-raderna ovan. Rätta gärna om jag har missuppfattat, annars helt OK att titta bort)
-'cin.ignore'
-    → ignorera lagrad character input
-'numeric_limits<streamsize>::max()'
-    →   ... av datatyper (numeric_limits) från största möjliga (max) dataflöde (streamsize) 
-        ... tills vi når '\n' (alltså där användaren matade in ENTER).
-Jag hade kunnat skriva "cin.ignore(100)", men genom att använda max() så behöver vi inte 
-oroa oss för att användaren skulle ange över 100 i length.
-Jag testade att klistra in hela Hamlet (ja, boken) som input, efter att ha tryckt 
-ENTER så avvärjade den det lika snabbt som en normalstor input, coolt :o */
-            continue; // lämna detta block och hoppa tillbaka upp till toppen av loopen igen/ta fram menyn
+        std::cout 
+            << ".:HUVUDMENY:.\n"
+            << "1. Minimeny A\n"
+            << "2. Minimeny B\n"
+            << "3. Avsluta\n\n\tSvar:\t";
+        std::cin >> menyVal;
+
+        switch (menyVal) {
+            case 1: 
+                minimenyA(); 
+                break;
+            case 2: 
+                minimenyB(); 
+                break;
+            case 3:
+                std::cout << "\n\t... Avslutar ...\n";
+                return 0;
+            default:
+                // std::cout << "Felaktigt val!\n";
+                felInput();
         }
-        if (val == 5) {
-            std::cout << "\n\tOK\n\tHEJDÅ\n";
+    }
+}
+void felInput(){
+    int menyVal = menyVal;
+    if (std::cin.fail() || menyVal < 1 || menyVal > 5) { // Här hamnar vi vid felaktig input (t ex 'abc') ELLER om input är mindre än 1 ELLER större än 5
+        std::cout   
+        << "::::::::::::::::::::::::::::::::::::::::\n"
+        << "Det där är inte ett alternativ mellan 1-3 :/\n\t"
+        << "Försök igen\n"
+        << "::::::::::::::::::::::::::::::::::::::::\n";
+        std::cin.clear();   // Reset:ar flag för "dålig character input" 
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+    }
+}
+void minimenyA() {
+    int menyA;
+    int buffertA = 0;
+    while (true) {
+        std::cout << "\n\t.:Minimeny A:.\n"
+                  << "\t1. Skapa värden\n"
+                  << "\t2. Visa statistik\n"
+                  << "\t3. Tillbaka till huvudmeny\n\n\t\tSvar:\t";
+        std::cin >> menyA;
+
+        if (menyA == 3) {
+            break;
+    }
+        if (menyA == 2 && buffertA == 0) {
+            std::cout << "Det finns ingen data att visa statistik på!\nGenomför alternativ '2. Skapa värden' först.\n";
+            continue;
+        }
+
+        if (menyA == 1) {
+            std::cout << "\n\t\t1. Ange 5 värden\n\t\t2. Generera 5 random-värden\n";
+            std::cin >> menyA;
+            buffertA = 1;
+        }
+    }
+}
+void minimenyB() {
+    int menyB;
+    while (true) {
+        std::cout << "\n\t.:Minimeny B:.\n"
+                  << "\t1. Annan grej 1\n"
+                  << "\t2. Annan grej 2\n"
+                  << "\t3. Tillbaka till huvudmeny\n\n\t\tSvar:\t";
+        std::cin >> menyB;
+
+        if (menyB == 3) {
             break;
         }
-        switch (val) {
-            case 1: {   // Addition
-                std::string oper = inputOperations();
-                result = input1 + input2;
-                std::cout << "\n" << oper << "\nResultat:\t" 
-                << input1 << " " << symbol << " " << input2 << " = " << result << "\n\n";
-                break;
-            }
-            case 2: {   // Subtraktion
-                std::string oper = inputOperations();
-                result = input1 - input2;
-                std::cout << "\n" << oper << "\nResultat:\t" 
-                << input1 << " " << symbol << " " << input2 << " = " << result << "\n\n";
-                break;
-            }
-            case 3: {   // Multiplikation
-                std::string oper = inputOperations();
-                result = input1 * input2;
-                std::cout << "\n" << oper << "\nResultat:\t" 
-                << input1 << " " << symbol << " " << input2 << " = " << result << "\n\n";
-                break;
-            }
-            case 4: {   // Division
-                std::string oper = inputOperations();
-                if (input1 == 0 && input2 == 0) {   // Hantera 0/0
-                    while (true) {  //  Straff i form av obehaglig loop utan break
-                        std::cout << "( ctrl + c )\nVAD\tHAR\tDU\tGJORT\t?\t!\tskäms!\t";
-                    }
-                } else {
-                    result = input1 / input2;
-                    std::cout << "\n" << oper << "\nResultat:\t" 
-                    << input1 << " " << symbol << " " << " = " << result << "\n\n";
-                }
-                break;
-            }
-        }
     }
-    return 0;
 }
-
-        // Static - kika närmare på
